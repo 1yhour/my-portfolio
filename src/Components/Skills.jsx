26464af8b05fa71e3cd59f53c886620ce6b542e6
LYHOUR SENG <lyhourcoding@gmail.com>
2025-12-19 18:21:23 +0700

    Added Skills Component

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -10,7 +10,7 @@ const Skills = () => {
                 {/*This is called Destructure extracting values from objects or arrays into separate variables in a clean, short way*/}
                 <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8 mx-auto lg:max-w-5xl">
                     {skills.map(({ name, percentage, icon: SkillIcon, color }, index) => (
-                        <div key={index} className="bg-gray-800 p-6 rounded-xl shadow-sm hover:shadow-md transition-shadow px-8">
+                        <div key={index} className="bg-white border-2 border-black p-6 rounded-xl shadow-sm hover:shadow-md transition-shadow px-8">
                             {/* Top Row: Icon and Name */}
                             <div className="flex items-center">
                                 <div className="flex-shrink-0">
@@ -19,12 +19,12 @@ const Skills = () => {
                                 <div className="flex flex-col ml-3 w-full ">
                                     <div className="font-inter_regular text-md mb-2">
                                         <div className="flex justify-between items-center">
-                                            <h3 className="text-white">{name}</h3>
-                                            <span className="text-gray-300">{percentage}%</span>
+                                            <h3 className="text-black">{name}</h3>
+                                            <span className="text-gray-800">{percentage}%</span>
                                         </div>
                                     </div>
                                     <div className="relative bg-orange-500 h-2 rounded-full">
-                                        <div className="absolute h-full rounded-full bg-white top-0 left-0 transition-all duration-1000"
+                                        <div className="absolute h-full rounded-full bg-black top-0 left-0 transition-all duration-1000"
                                              style={{width: `${percentage}%`}}>
                                             <div className="absolute -right-2 -top-1.5 h-5 w-5 rounded-full bg-black border-4 border-gray-100 shadow-lg"></div>
                                         </div>

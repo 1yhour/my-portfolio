6ea45d25062c42266244a1d4ce6839c6b2d05d7a
LYHOUR SENG <lyhourcoding@gmail.com>
2025-12-19 18:22:26 +0700

    Added Snowfall component

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,26 @@ import Header from "./Components/Header.jsx";
 import Hero from "./Components/Hero.jsx";
 import About from "./Components/About.jsx";
 import Skills from "./Components/Skills.jsx";
+import Snowfall from "react-snowfall";
+import Projects from "./Components/Projects.jsx";
 const App = () => {
     return (
         <div className="min-h-screen bg-white">
             <Header />
             <main>
+                <Snowfall color="#dee4fd"
+                          style={{
+                              position: "fixed",
+                              width: "100vw",
+                              height: "100vh",
+                              zIndex:1,
+                          }}
+                          snowflakeCount={150}
+                />
                 <Hero/>
                 <About/>
                 <Skills/>
+                <Projects/>
             </main>
         </div>
     )

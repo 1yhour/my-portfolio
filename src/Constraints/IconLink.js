15d17073815bb94f36234c2d7e14be5e1b4b5564
LYHOUR SENG <lyhourcoding@gmail.com>
2025-12-26 12:22:18 +0700

    Update link profile

--- src/Constraints/IconLink.js
+++ src/Constraints/IconLink.js
@@ -1,15 +1,14 @@
 import {FaGithub, FaLinkedin} from "react-icons/fa";
-import {MdMail} from "react-icons/md";
 export const iconLink = [
     {
         id: 1,
-        link:"https://github.com",
+        link:"https://github.com/1yhour",
         icon: FaGithub,
         color: "text-gray-900 hover:text-gray-800"
     },
     {
         id: 2,
-        link: "https://www.linkedin.com/in/seng-lyhour-607851383/",
+        link: "https://www.linkedin.com/in/seng-lyhour-607051383/",
         icon: FaLinkedin,
         color: "text-blue-600 hover:text-blue-500"
     },
